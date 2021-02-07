9b39731f9ef4313260591aad8f3b0388fb7ea42d
Randy Ikhsan Ramadhan <52363178+randyir@users.noreply.github.com>
2021-02-07 19:24:02 +0700

    Add files via upload

---
+++ Reamur.java
@@ -0,0 +1,16 @@
+// KELOMPOK 5
+package konversisuhu;
+public class Reamur {
+    double toFahrenheit(){
+        //°F = °Ré × 2,25 + 32
+        return (KonversiSuhu.SuhuAwal*2.25+32);
+    }
+    double toKelvin(){
+        //K = °Ré / 0,8 + 273,15
+        return (KonversiSuhu.SuhuAwal/0.8+273.15);
+    }
+    double toCelcius(){
+        //°C = °Ré / 0,8
+        return (KonversiSuhu.SuhuAwal/0.8);
+    }
+}
